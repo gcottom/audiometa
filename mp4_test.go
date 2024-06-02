8693fce9919d4a2318654d246e2591437965d451
Gage Cottom <gagecottom@hotmail.com>
2024-06-01 22:38:53 -0500

    update pipeline. add mp4 testss

--- mp4_test.go
+++ mp4_test.go
@@ -1,6 +1,9 @@
 package audiometa
 
 import (
+	"image"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/abema/go-mp4"
@@ -102,4 +105,41 @@ func TestGetAtomsList(t *testing.T) {
 		{'\251', 'd', 'a', 'y'},
 	}
 	assert.ElementsMatch(t, expected, result)
+}
+
+func TestMetadataGetFunctions(t *testing.T) {
+	imgPath, err := filepath.Abs("./testdata/withAlbumArt/testdata-img-1.jpg")
+	assert.NoError(t, err)
+	f, err := os.Open(imgPath)
+	assert.NoError(t, err)
+
+	imgData, _, err := image.Decode(f)
+	assert.NoError(t, err)
+
+	m := metadataMP4{
+		data: map[string]interface{}{"\xa9day": "2008-08-09", "\xa9cmt": "a comment about comments", "covr": &imgData, "intData": 6},
+	}
+	t.Run("test getInt", func(t *testing.T) {
+		intData := m.getInt([]string{"intData"})
+		assert.Equal(t, 6, intData)
+	})
+	t.Run("test getYear", func(t *testing.T) {
+		year := m.year()
+		assert.Equal(t, 2008, year)
+	})
+	t.Run("test getComment", func(t *testing.T) {
+		cmnt := m.comment()
+		assert.Equal(t, "a comment about comments", cmnt)
+	})
+	t.Run("test getPicture", func(t *testing.T) {
+		pic := m.picture()
+		picFile, err := os.Open(imgPath)
+		assert.NoError(t, err)
+		picData, _, err := image.Decode(picFile)
+		assert.NoError(t, err)
+		img1data := image_2_array_at(picData)
+		img2data := image_2_array_at(*pic)
+
+		assert.True(t, compareImages(img1data, img2data))
+	})
 }
